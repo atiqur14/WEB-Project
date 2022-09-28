9dd9d427ec4cf45cdda9430bf6c187d1298c243f
Atiqur Rahman <106583644+atiqur14@users.noreply.github.com>
2022-09-29 02:04:49 +0600

    Add files via upload

---
+++ client/src/screens/LandingScreen.js
@@ -0,0 +1,27 @@
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+import { Link } from "react-router-dom";
+// ..
+AOS.init({
+  duration: 2000,
+});
+
+function LandingScreen() {
+  return (
+    <div className="row landing">
+      <div className="col-md-12 text-center">
+        <h2 data-aos="zoom-in" style={{ color: "white", fontSize: "100px" }}>
+          Bengal Online Hotel Booking
+        </h2>
+        <h1 data-aos="zoom-out" style={{ color: "white" }}>
+          There is only one boss. The Guest.
+        </h1>
+        <Link to="/home">
+          <button className="btn btn-primary landingBtn">Get Started</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default LandingScreen;
